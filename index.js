c4274eb469a8c8aa0e1c23e0a29c0f408fbbb7e8
Mujakkir85 <mujakkirlucse@gmail.com>
2022-05-24 18:48:50 +0600

    added jwt

--- index.js
+++ index.js
@@ -15,12 +15,17 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+
 async function run() {
 
     try {
         await client.connect();
         console.log("DataBase Connected!")
-        //const Collection = client.db('truck_parts').collection('parts')
+        //const partsCollection = client.db('truck_parts').collection('parts')
+        const usersCollection = client.db('truck_parts').collection('users')
+
+
 
     }
     finally {
